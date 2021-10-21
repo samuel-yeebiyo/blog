39002a9271eb5cd19f9d74f6352b9d04e3866dd9
Samuel <samoyemaneyy@gmail.com>
2021-10-21 05:57:38 +0000

    Created some components and also finished with the nav bar

---
+++ src/components/resusable/Nav.js
@@ -0,0 +1,20 @@
+import '../../css/nav.css'
+
+const Nav = ()=> {
+    return (
+        <div className="nav-bar">
+            <div className="logo">
+                <p>Blogs.AM</p>
+            </div>
+            <div className="nav-menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Articles</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
